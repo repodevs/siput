0c3450fe49c5325d97bfe801eb07418faa3c1fd0
repodevs <repopamor@gmail.com>
2015-12-06 22:58:32 +0700

    updated feedback index input reports usulan

--- sys/connect_db.php
+++ sys/connect_db.php
@@ -1,7 +1,7 @@
 
 <?php
 $username = "root";
-$password = "qweasd";
+$password = "";
 $hostname = "localhost"; 
 //connection to the database
 $dbhandle = mysql_connect($hostname, $username, $password) or die("Unable to connect to MySQL");
